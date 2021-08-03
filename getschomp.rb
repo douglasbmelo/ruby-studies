59d78107a69cca33800804c332fa5d0031b39b9e
Douglas Melo <douglas.bmelo@gmail.com>
2021-08-03 09:39:02 -0300

    learning about array

--- getschomp.rb
+++ getschomp.rb
@@ -1,4 +1,9 @@
+names = []
 puts 'Olá, qual é o seu nome?'
-name = gets.chomp
-puts 'Seu nome é ' + name + '?  Que nome bonito!'
-puts 'Prazer em conhecê-lo, ' + name + '.  :)'
+names << gets.chomp
+puts 'Olá, qual é o seu nome?'
+names << gets.chomp
+puts 'Seu nome é ' + names[0] + '?  Que nome bonito!'
+puts 'Prazer em conhecê-lo, ' + names[0] + '.  :)'
+puts 'Seu nome é ' + names[1] + '?  Que nome bonito!'
+puts 'Prazer em conhecê-lo, ' + names[1] + '.  :)'
