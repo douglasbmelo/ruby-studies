9f6924b50ee6639e7d9d8f7e473bbf09d4ede9da
Douglas Melo <douglas.bmelo@gmail.com>
2021-08-10 06:23:49 -0300

    juntei hipotenusa, area e perimetro

--- hipot.rb
+++ hipot.rb
@@ -1,7 +1,9 @@
-lados = []
+catetos = []
 puts 'Qual o lado A do triâgulo?'
-lados << gets.chomp.to_i
+catetos << gets.chomp.to_i
 puts 'Qual o lado B do triâgulo?'
-lados << gets.chomp.to_i
-a = Math.sqrt ((lados[0].to_i) **2 + (lados[1].to_i) **2)
-puts 'A hipotenusa é: '+a.to_s
+catetos << gets.chomp.to_i
+hipotenusa = Math.sqrt((catetos[0])**2 + (catetos[1])**2)
+puts "A hipotenusa é: #{hipotenusa}"
+puts "A área é: #{(catetos[0] * catetos[1])/2}"
+puts "O perimetro é: #{catetos[0] + catetos[1] + hipotenusa}"
