29ce14b97032fe5164e20b371386298f925a497f
Douglas Melo <douglas.bmelo@gmail.com>
2021-08-10 07:30:44 -0300

    test

--- hipot.rb
+++ hipot.rb
@@ -7,3 +7,5 @@ hipotenusa = Math.sqrt((catetos[0])**2 + (catetos[1])**2)
 puts "A hipotenusa é: #{hipotenusa}"
 puts "A área é: #{(catetos[0] * catetos[1])/2}"
 puts "O perimetro é: #{catetos[0] + catetos[1] + hipotenusa}"
+
+
