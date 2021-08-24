15d8324bd47c4e55815a6615ee143b003cf2312e
Douglas Melo <douglas.bmelo@gmail.com>
2021-08-24 07:03:19 -0300

    metodo

---
+++ array.rb
@@ -0,0 +1,4 @@
+arr = ['fork','knife','spoon', 'caveats']
+arr.each_with_index {|item, banana| puts "#{banana}: #{item}"}
+array2 = [1,2,3,4]
+array2.each_with_index {|item, index|puts item + index}
---
+++ metodo.rb
@@ -0,0 +1,9 @@
+def nome_completo(primeiro, segundo)
+    puts "#{primeiro} #{segundo}"
+end
+
+nome_completo 'douglas', 'melo'
+nome_completo('kassio', 'borges')
+a = "fulano"
+b = "siclano"
+nome_completo a, b
