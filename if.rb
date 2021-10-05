6a3dc6bc11d3b6639f5f00c681f0492af1855e7e
Douglas Melo <douglas.bmelo@gmail.com>
2021-10-04 21:54:01 -0300

    Area do triangulo

--- if.rb
+++ if.rb
@@ -1,6 +1,10 @@
-def area_circulo(raio, pi = 3.14)     
-    pi * raio**2     
+def area_circulo(raio)     
+    Math::PI * raio**2     
 end
+
+def area_triangulo(base, altura)
+    (base * altura) / 2
+end    
 
 puts 'Qual o objeto deseja calcuçar a área?'
 object = gets.chomp
@@ -9,7 +13,11 @@ if object == 'circulo'
     raio = gets.chomp.to_i
     puts area_circulo(raio)
 elsif object == 'triangulo'
-    puts 2
+    puts 'Qual é a base do Triângulo?'
+    base = gets.chomp.to_i
+    puts 'Qual é a altura do Triângulo?'
+    altura = gets.chomp.to_i
+    puts area_triangulo(base, altura)    
 else 
     puts 'Objeto não encontrado'
 end  
