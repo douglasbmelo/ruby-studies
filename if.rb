2e7913d16b568063330e9dc8278b349ad07edf59
Douglas Melo <douglas.bmelo@gmail.com>
2021-10-04 21:58:31 -0300

    espaçamento

--- if.rb
+++ if.rb
@@ -8,16 +8,19 @@ end
 
 puts 'Qual o objeto deseja calcuçar a área?'
 object = gets.chomp
+
 if object == 'circulo'
     puts 'Qual é o tamanho do raio?'
     raio = gets.chomp.to_i
     puts area_circulo(raio)
+
 elsif object == 'triangulo'
     puts 'Qual é a base do Triângulo?'
     base = gets.chomp.to_i
     puts 'Qual é a altura do Triângulo?'
     altura = gets.chomp.to_i
-    puts area_triangulo(base, altura)    
+    puts area_triangulo(base, altura) 
+
 else 
     puts 'Objeto não encontrado'
 end  
