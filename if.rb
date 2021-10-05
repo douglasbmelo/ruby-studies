a9ca84ba93d1d10b03e0f3eb6c2ead2107586a02
Douglas Melo <douglas.bmelo@gmail.com>
2021-10-04 22:13:15 -0300

    acrescentei o prisma

--- if.rb
+++ if.rb
@@ -6,6 +6,32 @@ def area_triangulo(base, altura)
     (base * altura) / 2
 end    
 
+def get_prisma_data(i)
+    prisma = []
+    puts "Whats the side a of the prisma? #{i}"
+    prisma << gets.chomp.to_i
+    puts "Whats the side b of the prisma? #{i}"
+    prisma << gets.chomp.to_i
+    puts "Whats the height c of prisma? #{i}"  
+    prisma << gets.chomp.to_i
+  end
+  
+  def prisma_diagonal_base(prisma)
+    Integer.sqrt((prisma[0]**2) + (prisma[1]**2))
+    
+  end
+
+  def diagonal_do_prisma(prisma)
+    Integer.sqrt((prisma_diagonal_base(prisma))**2 + (prisma[2])**2)
+  end
+    
+  def do_prisma(i)
+    prisma = get_prisma_data(i)
+  
+    puts "Diagonal da base é: #{prisma_diagonal_base(prisma)}"
+    puts "Diagonal do prisma é: #{diagonal_do_prisma(prisma)}"
+  end
+
 puts 'Qual o objeto deseja calcuçar a área?'
 object = gets.chomp
 
@@ -21,7 +47,13 @@ elsif object == 'triangulo'
     altura = gets.chomp.to_i
     puts area_triangulo(base, altura) 
 
+elsif object == 'prisma'
+    puts "How many prismas?"
+    quantity = gets.chomp.to_i
+    
+    quantity.times {|i| do_prisma(i)}
 else 
+    
     puts 'Objeto não encontrado'
 end  
 
