4cf9757f9b0549eaeaa018f7069b4e1e8128496c
Douglas Melo <douglas.bmelo@gmail.com>
2021-11-23 07:43:10 -0300

    Parar jogo e correções

--- velha.rb
+++ velha.rb
@@ -10,14 +10,18 @@ class Velha
   def comecar
     positions = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
     puts TEMPLATE % positions
-    
+  
     9.times do |round|  
       symbol = SYMBOLS[round%2]
       puts "Jogador #{symbol}: informa qual posição irá jogar"
       input = gets.chomp.to_i
       positions[input - 1] = symbol
       puts TEMPLATE % positions
-      row_winner(positions)
+      winner = row_winner(positions)
+      if winner != nil
+        puts "O jogador #{winner} ganhou!"
+        break
+      end  
     end
   end
 
@@ -27,28 +31,18 @@ class Velha
   # são x OU são o
   def row_winner(positions)
     primeira_linha = positions[0, 3]
-    segunda_linha = positions[4, 6]
-    terceira_linha = positions[7, 9]
+    segunda_linha = positions[3, 3]
+    terceira_linha = positions[6, 3]
 
-    if primeira_linha.all?('x') 
-      puts "jogador x ganhou"
-    elsif primeira_linha.all?('o')
-      puts "jogador o ganhou"
-    end  
-
-    if segunda_linha.all?('x') 
-      puts "jogador x ganhou"
-    elsif segunda_linha.all?('o')
-      puts "jogador o ganhou"
-    end 
-
-    if terceira_linha.all?('x') 
-      puts "jogador x ganhou"
-    elsif terceira_linha.all?('o')
-      puts "jogador o ganhou"
+    if primeira_linha.all?('x') || segunda_linha.all?('x') || terceira_linha.all?('x')
+      "x"
+    elsif primeira_linha.all?('o') || segunda_linha.all?('o') || terceira_linha.all?('o')
+      "o"
     end    
   end
 end
 
 @jogo = Velha.new
 
+#iniciar arquivo no irb usar '' string
+#comecar o jogo no irb @jogo.comecar
